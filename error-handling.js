75c90a7fb7eec2ed89428d75e0a15d35784cd923
StephanyKleen <stephanykleen@gmail.com>
2023-07-24 16:51:05 -0700

    Testing Erro handling

--- error-handling.js
+++ error-handling.js
@@ -1,20 +1,49 @@
-// 1.
+/*// 1.
 function sum(array) {
+
+  if(!Array.isArray(array)) {
+    throw new Error("this is not an array!");
+  } else{
   let sum = 0;
   for (let i = 0; i < array.length; i++) {
     sum += array[i];
   }
   return sum;
+  }
+ 
 }
-
 let res = sum(null);
-console.log(res);
+//console.log(res);
+//----------------------------------//
+try{
+  console.log(res)
+}catch(error){
+  console.error(`Wrong Type: ${error.message}`)}
+
+
+
 
 // 2.
-// tests
-sayName("Alex");
-sayName(1);
-// Your code here
+
+const sayName = (name) =>{
+  if(typeof name === 'string'){console.log(name)}
+  else{
+    throw Error("Invalid name! Must be a string!")
+  }
+}
+
+
+
+//----------------------------------//
+try{
+  sayName("Alex");
+  sayName(1);
+}catch(error){
+
+    console.error(`TypeError ${error.message}`)
+
+ }
+*/
 
 // 3.
 function greet(greeting) {
@@ -23,4 +52,10 @@ function greet(greeting) {
   }
 
   console.log(greeting);
+}
+
+try{
+  console.log(greet())
+}catch(e){
+  console.log("Hello World!")
 }
